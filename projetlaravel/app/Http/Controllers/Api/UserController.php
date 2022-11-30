2b4584c14cae9a21240211f746c268d7994f09e4
CHEIKH <chmouhammed260594@gmail.com>
2022-11-30 19:36:09 +0000

    link entre front et back

---
+++ projetlaravel/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+        $users = User::all();
+
+        return response()->json($users);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+}
