9cf401a17968a681f402644aba5962fb79e5c69c
fatima <mamefatimandiaye87@gmail.com>
2022-12-02 10:40:14 +0000

    control ok

--- projetlaravel/routes/web.php
+++ projetlaravel/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\Utilisateurs;
+use App\Http\Controllers\login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}) ->name('welcome');
+Route::get('/login', [App\Http\Controllers\TestController::class,'index'])->name('login');
 
+Route::post('/login/save', [App\Http\Controllers\TestController::class,'store'])->name('login.store');
