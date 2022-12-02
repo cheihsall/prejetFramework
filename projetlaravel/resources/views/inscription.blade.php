fd294d0ef95f89cfb66d6fecb7ddfe8ffee30a89
LAYE MAKHTAAR <adama jeey>
2022-12-02 15:22:54 +0000

    push

--- projetlaravel/resources/views/inscription.blade.php
+++ projetlaravel/resources/views/inscription.blade.php
@@ -11,67 +11,74 @@
 </head>
 
 <body>
-    <div class="container d-flex justify-content-center mt-4">
-        <div class="col-md-8  mt-4">
-            <br>
-
-            <nav class="navbar navbar-dark bg-success mt-4">
-                <div class="container-fluid d-flex justify-content-center">
-                    <a class="navbar-brand pe-none" href="#">
-                        <b> FORMULAIRE INSCRIPTION</b>
-                    </a>
-                </div>
-            </nav>
-
-            <form id="form" enctype="multipart/form-data" class="row g-3 d-flex justify-content-center no-wrap m-2  bg-light needs-validation" novalidate action="inscription.php" method="post">
+    <div class="container d-flex justify-content-center mt-5">
+        <div class="col-md-8  mt-5">
+            <form id="form" enctype="multipart/form-data" class="row g-3 d-flex justify-content-center no-wrap m-2  bg-light needs-validation border" novalidate action="/inscription" method="POST">
+                @csrf
+                <nav class="navbar navbar-dark bg-success mt-0">
+                    <div class="container-fluid d-flex justify-content-center">
+                        <a class="navbar-brand pe-none" href="#">
+                            <b> FORMULAIRE INSCRIPTION</b>
+                        </a>   
+                    </div>
+                </nav>
                 <div class="col-md-6 input-control">
                     <label for="input1" class="form-label">Nom<span style="color: red;">*</span></label>
-                    <input type='text' name='nom' class="form-control border-dark p-2" id="nom" required>
-                    <div class="invalid-feedback d-none" id="erreur_nom"> Nom est obligatoire</div>
+                    <input type='text' name='nom' class="form-control border-dark p-2 @error('nom') is-invalid @enderror" id="nom" required>
+                    @error('nom')
+                    <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="col-md-6 input-control">
                     <label for="input2" class="form-label">Prenom<span style="color: red;">*</span></label>
-                    <input type="text" class="form-control border-dark p-2" name="prenom" id="prenom" required>
-                    <div class="invalid-feedback d-none" id="erreur_prenom">Prenom est obligatoire</div>
+                    <input type="text" class="form-control border-dark p-2 @error('prenom') is-invalid @enderror" name="prenom" id="prenom" required> 
+                    @error('prenom')
+                    <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="col-md-6 input-control">
                     <label for="input3" class="form-label">Email<span style="color: red;">*</span></label>
-                    <input type="text" class="form-control border-dark p-2" name="email" id="email" required>
-                    <div class="invalid-feedback d-none" id="erreur_email">Email est obligatoire</div>
-                    <div class="invalid-feedback d-none" id="erreur_email2">Entrez un format valide</div>
+                    <input type="text" class="form-control border-dark p-2 @error('email') is-invalid @enderror" value="{{old('email')}}" name="email" id="email" required>
+                    @error('email')
+                    <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="col-md-6 input-control">
                     <label for="input7" class="form-label">Rôles<span style="color: red;">*</span></label>
-                    <select name="roles" class="form-select border-dark p-2" id="roles" required>
+                    <select name="roles" class="form-select border-dark p-2 @error('roles') is-invalid @enderror" id="roles" required>
                         <option selected disabled value=""></option>
                         <option value="administrateur" name='roles'>Admin</option>
                         <option value="utilisateur" name='roles'>User</option>
                     </select>
-
-                    <div class="invalid-feedback d-none" id="erreur_roles">Rôle est obligatoire</div>
+                    @error('roles')
+                    <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="col-md-6 input-control">
                     <label for="input5" class="form-label">Mot de passe<span style="color: red;">*</span></label>
-                    <input type="password" class="form-control border-dark p-2" name="passwords" id="passwords" required>
-                    <div class="invalid-feedback d-none" id="erreur_passwords">mot de passe est obligatoire</div>
+                    <input type="password" class="form-control border-dark p-2 @error('passwords') is-invalid @enderror" name="passwords" id="passwords" required>
+                    @error('passwords')
+                    <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="col-md-6 input-control">
                     <label for="input6" class="form-label">Confirme mot de passe<span style="color: red;">*</span></label>
-                    <input type="password" class="form-control border-dark p-2" name="passwords" id="passwords2" required>
-                    <div class="invalid-feedback d-none" id="erreur_passwords2">veuillez confirmer votre mot de passe</div>
-                    <div class="invalid-feedback d-none" id="confpasswords">mot de passe non identiques</div>
+                    <input type="password" class="form-control border-dark p-2 @error('passwords') is-invalid @enderror" name="passwords" id="passwords2" required>
+                    @error('passwords')
+                    <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div class="col-md-6 input-control">
                     <label for="input4" class="form-label">Photo de profil</label>
                     <input type="file" name="photo" accept=".jpg,.png,.jpeg" class="form-control border-dark p-2">
                 </div>
-
+                 
                 <div class="row d-flex justify-content-center mt-2">
                     <button type="submit" class="btn btn-success col-3" id="submit">
                         S'inscrire
--- projetlaravel/routes/web.php
+++ projetlaravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\Utilisateurs;
+use App\Models\utilisateur;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('inscription/', function () {
+Route::get('/inscription', function () {
     return view('inscription');
 });
-
+ 
+Route::post('/inscription' ,[utilisateurs::class,'inscription']);
