4c11907d19ddd5b00f4edb5d6ed1215f5243da36
fatima <mamefatimandiaye87@gmail.com>
2022-12-05 13:35:03 +0000

    cc

---
+++ projetlaravel/.history/app/Http/Controllers/Utilisateurs_20221205130150.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use App\Models\utilisateur;
+use Illuminate\Http\Request;
+
+class Utilisateurs extends Controller
+{
+
+
+    //controle formulaire
+ 
+    public function inscription(Request $request){
+
+        return $request->all();
+
+
+        $validation = $request->validate([
+
+            'nom' => 'required',
+            'prenom' => 'required',
+            'email' => 'required | regex: /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
+            'passwords' => 'required',
+            'roles' => 'required',
+            'passwords2' => 'required',
+            
+        ]);
+        return $validation;
+
+
+        
+    }
+     //controle de saisie login 
+
+    public function login(Request $request){
+
+        $email = $request->get('email');
+        $mdp = $request->get('passwords');
+
+
+        $valid = $request->validate([
+            'email' => ['required', 'email','regex: /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/'],
+            'passwords' => 'required', 'string',   
+        ]);
+
+       $utilisateur= Utilisateur::where("email",$valid["email"])->first();
+       $pass= Utilisateur::where("passwords",$valid["passwords"])->first();
+       //
+       if(!$utilisateur) return response(["message"=>"l'email n'existe pas"]);
+       if ($utilisateur['passwords'],$utilisateur->passwords)) response(["message"=>"mdp incorrect"]);
+       //
+       
+       return $utilisateur;
+
+
+        
+    }
+}
+ ?> 
+
---
+++ projetlaravel/.history/app/Http/Controllers/Utilisateurs_20221205130943.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use App\Models\utilisateur;
+use Illuminate\Http\Request;
+
+class Utilisateurs extends Controller
+{
+
+
+    //controle formulaire
+ 
+    public function inscription(Request $request){
+
+        return $request->all();
+
+
+        $validation = $request->validate([
+
+            'nom' => 'required',
+            'prenom' => 'required',
+            'email' => 'required | regex: /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
+            'passwords' => 'required',
+            'roles' => 'required',
+            'passwords2' => 'required',
+            
+        ]);
+        return $validation;
+
+
+        
+    }
+     //controle de saisie login 
+
+    public function login(Request $request){
+
+        $email = $request->get('email');
+        $mdp = $request->get('');
+
+
+        $valid = $request->validate([
+            'email' => ['required', 'email','regex: /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/'],
+            'passwords' => 'required', 'string',   
+        ]);
+
+       $utilisateur= Utilisateur::where("email",$valid["email"])->first();
+       $pass= Utilisateur::where("motdepasse",$valid["motdepasse"])->first();
+       //
+       if(!$utilisateur ) return response(["message"=>"l'email n'existe pas"]);
+       /* if (!Hash::check($utilisateur['passwords'],$utilisateur->passwords)) response(["message"=>"mdp incorrect"]); */
+       //
+        if(!$pass ) return response(["message"=>"pass n'existe pas"]); 
+       return $utilisateur;
+
+
+        
+    }
+}
+ ?> 
+
