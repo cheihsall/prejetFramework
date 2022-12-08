a5f466e8d3f131b1e3a96c24bfad3fd876ddd9d6
mariemesow <Your_Email_Addressmariecherifsow@gmail.com>
2022-12-08 18:22:35 +0000

    rech

---
+++ .history/projetlaravel/resources/js/recherche_20221207143210.js
@@ -0,0 +1,8 @@
+function search(){
+    let recherche = document.getElementById('recherche');
+    let quit = document.getElementById('quit');
+
+    if (recherche.value !=" "){
+         quit.style.display = "block";
+
+    }
