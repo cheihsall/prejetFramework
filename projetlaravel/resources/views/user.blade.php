caeeaeb633530c77f16fed3ea4fa59b95397fd18
CHEIKH <chmouhammed260594@gmail.com>
2022-12-13 18:36:44 +0000

    retouch

--- projetlaravel/resources/views/user.blade.php
+++ projetlaravel/resources/views/user.blade.php
@@ -92,17 +92,6 @@
 </div>
 </div>
 </main>
-{{-- <script>
-    function search(){
-    let recherche = document.getElementById('recherche');
-    let quit = document.getElementById('quit');
 
-    if (recherche.value !=" "){
-         quit.style.display = "block";
-
-    }
-    }
-
-</script> --}}
 </body>
 </html>
