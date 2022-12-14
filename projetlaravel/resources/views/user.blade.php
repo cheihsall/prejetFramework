27bae19a9bc69d8f54a57865ce89e7c6f9cfcd14
LAYE MAKHTAAR <adama jeey>
2022-12-14 14:15:34 +0000

    env

--- projetlaravel/resources/views/user.blade.php
+++ projetlaravel/resources/views/user.blade.php
@@ -46,18 +46,9 @@
                 </a>
             </div>
             </form>
-           
+
      </div>
 
-
-    <div class="" style="margin-left:4rem; margin-top:-1rem;max-height: 2.5rem;">
-      <a href="/api/logout">
-
-
-        <button type="button" class="btn btn-outline-success "><i
-            class="fa-solid fa-arrow-right-from-bracket" style="color:white; font-size:35px; padding-top:12px;"></i>Deconnecter</button>
-    </a>
-            </div>
 
 </nav>
     </header>
